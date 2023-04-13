bf428ea4673623b13f32dca882f8e03fa88b7dea
Mike <124840291+mikewashereGH@users.noreply.github.com>
2023-04-13 08:34:33 -0400

    w

--- Assets/JS/home.js
+++ Assets/JS/home.js
@@ -4,3 +4,14 @@ const nav = document.getElementsByTagName("nav");
 navEl.addEventListener("click", () => {
     nav[1].classList.toggle("active");
 });
+
+const items = [
+    "Organize your thoughts with effortless ease.",
+    "Write it down, remember it forever.",
+    "The perfect place for your notes.",
+    "Notes made simple.",
+    "Capture thoughts, never miss a beat.",
+    "Your thoughts, organized and at ease."
+]
+const item = items[Math.floor(Math.random() * items.length)];
+document.getElementById("QouteMottoThingy").innerHTML = item;
